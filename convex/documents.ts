da651ca906c2c0e9cae7952dc26b0e4e2ab0d63b
metelitt <104203274+metelitt@users.noreply.github.com>
2023-12-18 21:11:01 +0300

    fix bug

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,6 +1,45 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { NormalizeError } from "next/dist/shared/lib/utils";
+import { Doc, Id } from "./_generated/dataModel";
+
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new NormalizeError("Не авторизован");
+    }
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) {
+      throw new Error("Не найдено");
+    }
+    if (existingDocument.userId !== userId) {
+      throw new Error("Не авторизован");
+    }
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArhived: true,
+        });
+        await recursiveArchive(child._id);
+      }
+    };
+    const document = await ctx.db.patch(args.id, {
+      isArhived: true,
+    });
+    return document;
+  },
+});
 
 export const getSidebar = query({
   args: {
