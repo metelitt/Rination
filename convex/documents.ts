0e068909c4038db929a2ca52beb97f03e0a92bff
metelitt <104203274+metelitt@users.noreply.github.com>
2023-12-19 17:12:46 +0300

    Trash

--- convex/documents.ts
+++ convex/documents.ts
@@ -37,6 +37,7 @@ export const archive = mutation({
     const document = await ctx.db.patch(args.id, {
       isArhived: true,
     });
+    recursiveArchive(args.id);
     return document;
   },
 });
@@ -85,6 +86,96 @@ export const create = mutation({
       isArhived: false,
       isPublished: false,
     });
+    return document;
+  },
+});
+
+export const getTrash = query({
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Не аутенфицирован");
+    }
+    const userId = identity.subject;
+    const document = await ctx.db
+      .query("documents")
+      .withIndex("by_user", (q) => q.eq("userId", userId))
+      .filter((q) => q.eq(q.field("isArhived"), true))
+      .order("desc")
+      .collect();
+    return document;
+  },
+});
+export const restore = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Не аутенфицирован");
+    }
+    const userId = identity.subject;
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Не авторизован");
+    }
+
+    const recursiveRestore = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArhived: false,
+        });
+        await recursiveRestore(child._id);
+      }
+    };
+
+    const options: Partial<Doc<"documents">> = {
+      isArhived: false,
+    };
+
+    if (existingDocument.parentDocument) {
+      const parent = await ctx.db.get(existingDocument.parentDocument);
+      if (parent?.isArhived) {
+        options.parentDocument = undefined;
+      }
+    }
+    const document = await ctx.db.patch(args.id, options);
+    recursiveRestore(args.id);
+    return document;
+  },
+});
+
+export const remove = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Не аутенфицирован");
+    }
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) {
+      throw new Error("Не найдено");
+    }
+    if (existingDocument.userId !== userId) {
+      throw new Error("Не авторизован");
+    }
+
+    const document = await ctx.db.delete(args.id);
     return document;
   },
 });
