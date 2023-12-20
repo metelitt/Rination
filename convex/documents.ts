561deca5f873c97dfc0b59620c0c1ad9d4eff37f
metelitt <104203274+metelitt@users.noreply.github.com>
2023-12-20 17:15:42 +0300

    fix bug

--- convex/documents.ts
+++ convex/documents.ts
@@ -179,3 +179,21 @@ export const remove = mutation({
     return document;
   },
 });
+export const getSearch = query({
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Не аутенфицирован");
+    }
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user", (q) => q.eq("userId", userId))
+      .filter((q) => q.eq(q.field("isArhived"), false))
+      .order("desc")
+      .collect();
+    return documents;
+  },
+});
