699f6505bbbedc7c860eaf6b76a9a90d824d50a2
metelitt <104203274+metelitt@users.noreply.github.com>
2023-12-23 18:38:51 +0300

    fix bug

---
+++ components/ui/toolbar.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Doc } from "@/convex/_generated/dataModel";
+import { IconPicker } from "./icon-picker";
+import { Smile, X } from "lucide-react";
+import { Button } from "./button";
+
+interface ToolberProps {
+  initialData: Doc<"documents">;
+  preview?: boolean;
+}
+
+export const Toolbar = ({ initialData, preview }: ToolberProps) => {
+  return (
+    <div className="pl-[54px] group relative">
+      {!!initialData.icon && preview && (
+        <div className="flex items-center gap-x-2 group/icon pt-6">
+          <IconPicker onChange={() => null}>
+            <p className="text-6xl hover:opacity-75 transition ">
+              {initialData.icon}
+            </p>
+          </IconPicker>
+          <Button
+            onClick={() => {}}
+            className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
+            variant="outline"
+            size="icon"
+          >
+            <X className="h-4 w-4 " />
+          </Button>
+        </div>
+      )}
+      {!!initialData.icon && preview && (
+        <p className="text-6xl pt-6">{initialData.icon}</p>
+      )}
+      <div className="opacity-100 group-hover:opacity-100 flex items-center gap-x-1 py-4">
+        {!initialData.icon && !preview && (
+          <IconPicker asChild onChange={() => {}}>
+            <Button
+              className="text-muted-foreground text-xs"
+              variant="outline"
+              size="sm"
+            >
+              <Smile className="h-4 w-4 mr-2" />
+              add Icon
+            </Button>
+          </IconPicker>
+        )}
+      </div>
+    </div>
+  );
+};
