ca21271d039e11ef72453e54ea835f74ab5ce5c4
metelitt <104203274+metelitt@users.noreply.github.com>
2023-12-24 10:14:20 +0300

    fix bug

--- components/icon-picker.tsx
+++ components/icon-picker.tsx
@@ -1,13 +1,20 @@
 "use client";
+
 import EmojiPicker, { Theme } from "emoji-picker-react";
 import { useTheme } from "next-themes";
-import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 interface IconPickerProps {
   onChange: (icon: string) => void;
   children: React.ReactNode;
   asChild?: boolean;
 }
+
 export const IconPicker = ({
   onChange,
   children,
